be7b4ea027ba80061963e0f5b919d8c0198731ad
prakasamajith <prakasamajith@gmail.com>
2024-09-14 09:55:39 +0530

    Codes added

--- src/test/java/org/demo/Tes.java
+++ src/test/java/org/demo/Tes.java
@@ -1,22 +1,9 @@
 package org.demo;
 
 public class Tes {
-
-	public Tes() {
-
-	System.out.println("Default con");
-	}
-
-	@Override
-	protected void finalize() throws Throwable {
-		System.out.println("Object des");
-	}
-public static void main(String[] args) {
-	Tes t=new Tes();
-	t=null;
-	System.gc();
 	
-}
-		
+	public Tes(int empId) {
+		System.out.println(empId);
+	}
 }
 
--- src/test/java/org/demo/Test.java
+++ src/test/java/org/demo/Test.java
@@ -1,26 +1,27 @@
 package org.demo;
 
-import java.util.Collection;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
 
 public class Test {
-	public static void main(String[] args) {
 
+public static void main(String[] args) {
+	
 	try {
 		int a=10/0;
 		System.out.println(a);
 	}
-	catch(ArithmeticException e){
-		e.printStackTrace();
+	catch(ArithmeticException d) {
+		d.printStackTrace();
 	}
 	finally {
-		System.out.println("This is in Finally Block");
+		System.out.println("i'm in Finally");
 	}
+
+} 
 	
-	
-	}
+
+
+
+
+
 
 }
